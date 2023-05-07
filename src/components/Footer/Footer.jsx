509c7880d693ebd7e7936589813bfcac5ec35a3a
Bindumuniyappa <muniyappabindu7@gmail.com>
2023-05-07 21:01:33 +0530

    modified

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -69,7 +69,7 @@ const Footer = () => {
         <Row className="mt-5">
           <Col lg="6" md="6">
             <p className="copyright__text">
-              Copyright - 2022, website made by Kava Ajay. All Rights
+              Copyright - 2023, website made by Bindushree. All Rights
               Reserved.
             </p>
           </Col>
